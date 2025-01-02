3e91987e4223b4e3bd6bc78f67e01195c1d4c912
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2025-01-01 21:49:43 -0400

    implementando modificadores de acesso

--- Typescript/typescript-curso-1-2696c7ca452f2eb6468caaccc2699da2332a39a1/app/views/negociacoes-view.ts
+++ Typescript/typescript-curso-1-2696c7ca452f2eb6468caaccc2699da2332a39a1/app/views/negociacoes-view.ts
@@ -4,7 +4,7 @@ import { View } from "./view.js";
 export class NegociacoesView extends View<Negociacoes>{
 
         
-    template (model:Negociacoes): string{
+    protected template (model:Negociacoes): string{
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -18,7 +18,7 @@ export class NegociacoesView extends View<Negociacoes>{
                 ${model.lista().map(negociacao =>{
                     return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                            <td>${this.dataConverter(negociacao.data)}</td>
                             <td>${negociacao.quantidade}</td>
                             <td>${negociacao.valor}</td>
                         </tr>
@@ -28,7 +28,10 @@ export class NegociacoesView extends View<Negociacoes>{
         </table>
         `;
     }
-    update (model: Negociacoes): void {
+    public update (model: Negociacoes): void {
         this.elemento.innerHTML = this.template(model);
+    }
+    private dataConverter(data:Date): string{
+        return new Intl.DateTimeFormat().format(data);
     }
 }
