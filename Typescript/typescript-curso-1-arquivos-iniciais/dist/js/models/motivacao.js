b526cd2caf0e386b82987da6b1adce1160397154
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2025-01-02 19:10:32 -0400

    adicionando módulos controller

---
+++ Typescript/typescript-curso-1-arquivos-iniciais/dist/js/models/motivacao.js
@@ -0,0 +1,19 @@
+export class Motivacao {
+    constructor(data, volume, quantidade) {
+        this._data = data;
+        this._volume = volume;
+        this._quantidade = quantidade;
+    }
+    get data() {
+        return this._data;
+    }
+    get volume() {
+        return this._quantidade * this._volume;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    Mensagem_motivacao() {
+        return "mensagem : *contatenação*";
+    }
+}
