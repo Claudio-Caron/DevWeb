f849726a12b9790b8f55d624975f5b60681eb888
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2025-01-08 09:51:43 -0400

    aprendizados em typescript e boas práticas
    ecorator com parâmetro
    -Criação de um decorator de inspeção
    -Ordem de execução dos decorators
    -Simplificação no design de decorators
    -Portabilidade de funcionalidade antiga para decorators

--- Typescript/typescript-curso-3-f4398d11cf4a7b0fd5adf9d387ce9e0f985242a0/app/src/controllers/negociacao-controller.ts
+++ Typescript/typescript-curso-3-f4398d11cf4a7b0fd5adf9d387ce9e0f985242a0/app/src/controllers/negociacao-controller.ts
@@ -10,7 +10,7 @@ export class NegociacaoController {
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
-    private negociacoesView = new NegociacoesView('#negociacoesView', true);
+    private negociacoesView = new NegociacoesView('#negociacoesView');
     private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
@@ -52,7 +52,7 @@ export class NegociacaoController {
         this.inputValor.value = '';
         this.inputData.focus();
     }
-
+   
     private atualizaView(): void {
         this.negociacoesView.update(this.negociacoes);
         this.mensagemView.update('Negociação adicionada com sucesso');
